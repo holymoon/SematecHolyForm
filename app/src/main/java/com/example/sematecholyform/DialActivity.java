3eddb219e452129db95ec90f67b88caf3b31c9fa
Houra <houra.mehdipour@gmail.com>
2019-10-21 16:33:23 +0330

    changes in dialactivity

--- app/src/main/java/com/example/sematecholyform/DialActivity.java
+++ app/src/main/java/com/example/sematecholyform/DialActivity.java
@@ -2,7 +2,12 @@ package com.example.sematecholyform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DialActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class DialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
+        Button btnDialNumber = findViewById(R.id.btnDialNumber);
+
+        btnDialNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
+                final String phoneNumber = edtPhoneNumber.getText().toString();
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:" +phoneNumber));
+                startActivity(i);
+            }
+        });
     }
 }
